a029f879e6ee59ff00417d3c0abb82e1fd586587
Steve Klabnik <steve@steveklabnik.com>
2011-05-06 16:48:44 -0400

    detect timeradd

--- vm/detection.hpp
+++ vm/detection.hpp
@@ -55,4 +55,28 @@ char *realpath(char *name, char *resolved){
 }
 #endif
 
+#ifndef HAS_REALPATH
+#define timeradd(a, b, result)                  \
+	do {                        \
+  	(result)->tv_sec = (a)->tv_sec + (b)->tv_sec;           \
+  		(result)->tv_usec = (a)->tv_usec + (b)->tv_usec;            \
+  		if ((result)->tv_usec >= 1000000)               \
+  		{                       \
+  			++(result)->tv_sec;                 \
+  				(result)->tv_usec -= 1000000;               \
+  		}                       \
+  } while (0) 
+
+#define timersub(a, b, result)                  \
+	do {                        \
+		(result)->tv_sec = (a)->tv_sec - (b)->tv_sec;           \
+		(result)->tv_usec = (a)->tv_usec - (b)->tv_usec;            \
+		if ((result)->tv_usec < 0) {                \
+			--(result)->tv_sec;                 \
+			(result)->tv_usec += 1000000;               \
+		}                       \
+	} while (0)
+
+
+#endif
 #endif
